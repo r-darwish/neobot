a3fc449eba7a12b22901b4f2a57cbff4e1d95781
Roey Darwish Dror <roey.ghost@gmail.com>
2013-01-11 13:43:54 +0200

    Tombola can parse the winning page

--- neopets/dailies/tombola.py
+++ neopets/dailies/tombola.py
@@ -7,7 +7,10 @@ class Tombola(object):
     _PLAY_BUTTON_ATTRS = dict(value='Play Tombola!')
     _POST_DATA = dict()
     _PRIZE_RE = re.compile('Your Prize - (.*)')
-    _SORRY_RE = re.compile(r'Sorry, you are only allowed one Tombola free spin every day')
+    _SORRY_RE = re.compile(
+        r'Sorry, you are only allowed one Tombola free spin every day')
+    _WINNER_RE = re.compile(r'YOU ARE A WINNER!!!')
+    _YOU_WIN_RE = re.compile(r'You Win (\d+) Neopoints')
 
     def __init__(self, account):
         self._logger = logging.getLogger(__name__)
@@ -34,13 +37,21 @@ class Tombola(object):
 
     def _on_submit(self, page):
         prize = page.find(text=self._PRIZE_RE)
-        if not prize:
+        winner = page.find(text=self._WINNER_RE)
+        if winner:
+            np = page.find(text=self._YOU_WIN_RE)
+            if not np:
+                raise PageParseError(page)
+
+            np = self._YOU_WIN_RE.search(np).group(1)
+            self._logger.info('Won %s NP and maybe some items', np)
+        elif prize:
+            prize = self._PRIZE_RE.search(prize).group(1)
+            self._logger.info('Prize is %s', prize)
+        else:
             if page.find(text='Oh dear, that\'s not a winning ticket :('):
                 self._logger.info('Didn\'t win')
             elif page.find(text=self._SORRY_RE):
                 self._logger.info('Already played today')
             else:
                 raise PageParseError(page)
-        else:
-            prize = self._PRIZE_RE.search(prize).group(1)
-            self._logger.info('Prize is %s', prize)
