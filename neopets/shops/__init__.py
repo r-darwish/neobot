5b0924eeb856f0574cbc2129c5fdab0cacdbe463
Roey Darwish Dror <roey.ghost@gmail.com>
2013-02-20 21:44:25 +0200

    Auction house moved to the shop object

--- neopets/shops/__init__.py
+++ neopets/shops/__init__.py
@@ -1,3 +1,1 @@
-from neopianshops import NeopianShops
 from shops import Shops
-from auctionhouse import AuctionHouse
--- neopets/shops/shops.py
+++ neopets/shops/shops.py
@@ -1,10 +1,16 @@
 from neopets.shops.neopianshops import NeopianShops, NeopianShop
 from neopets.shops.wizard import Wizard
+from neopets.shops.auctionhouse import AuctionHouse
 
 class Shops(object):
     def __init__(self, account):
         self._account = account
         self._wizard = Wizard(account)
+        self._auction_house = AuctionHouse(account)
+
+    @property
+    def auction_house(self):
+        return self._auction_house
 
     @property
     def wizard(self):
