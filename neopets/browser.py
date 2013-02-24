bc6274be6b73d24faed310b4d610f77ce7ab2516
Roey Darwish Dror <roey.ghost@gmail.com>
2013-02-24 22:22:54 +0200

    Use the new agent API

--- neopets/browser.py
+++ neopets/browser.py
@@ -1,21 +1,63 @@
 import os
 import logging
-import shelve
 from collections import namedtuple
 from urlparse import urlparse
 from urllib import urlencode
-from twisted.internet import reactor
-from twisted.web.client import getPage
+from twisted.internet.protocol import Protocol
+from twisted.internet import reactor, defer
+from twisted.web.client import Agent, CookieAgent
+from cookielib import LWPCookieJar, CookieJar
+from StringIO import StringIO
+from twisted.web.http_headers import Headers
+from zope.interface import implements
+from twisted.web.iweb import IBodyProducer
 
 
 RequestData = namedtuple('RequestData', ('url', 'referer', 'data'))
 
 
+class StringProducer(object):
+    implements(IBodyProducer)
+
+    def __init__(self, body):
+        self.body = body
+        self.length = len(body)
+
+    def startProducing(self, consumer):
+        consumer.write(self.body)
+        return defer.succeed(None)
+
+    def pauseProducing(self):
+        pass
+
+    def stopProducing(self):
+        pass
+
+
+class MemoryReceiver(Protocol):
+    def __init__(self):
+        self._finished = defer.Deferred()
+        self._io = StringIO()
+
+    @property
+    def finished(self):
+        return self._finished
+
+    def dataReceived(self, data):
+        self._io.write(data)
+
+    def connectionLost(self, _):
+        self._finished.callback(self._io.getvalue())
+        self._io.close()
+
+
 class Browser(object):
-    _AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17"
+    _HEADERS = {
+        'User-Agent' : ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17', ]
+    }
 
     _POST_HEADERS = {
-        'Content-Type' : 'application/x-www-form-urlencoded'
+        'Content-Type' : ['application/x-www-form-urlencoded', ]
     }
 
     def __init__(self, page_archiver, cookie_file=None):
@@ -26,64 +68,53 @@ class Browser(object):
                            cookie_file)
         if cookie_file:
             umask = os.umask(077)
-            self._cookies = shelve.open(cookie_file, writeback=False)
+            cj = LWPCookieJar(cookie_file)
             os.umask(umask)
         else:
-            self._cookies = dict()
+            cj = CookieJar()
 
-    def get(self, url, referer=None):
+        self._agent = CookieAgent(Agent(reactor), cj)
+
+    @defer.deferredGenerator
+    def _request(self, request_type, url, referer=None, body=None):
         self._logger.debug('Fetching %s', url)
 
-        headers = dict()
+        headers = dict(self._HEADERS)
         if referer:
-            headers['Referer'] = referer
+            headers['Referer'] = [referer, ]
 
-        d = getPage(
-            url,
-            headers=headers,
-            agent=self._AGENT,
-            cookies=self._cookies)
-        d.addCallback(self._on_download, RequestData(
-            urlparse(url), referer, None))
-        d.addErrback(self._on_error, url)
-        return d
+        body_prod = None
+        if body:
+            headers.update(self._POST_HEADERS)
+            body_prod = StringProducer(body)
+
+        d = defer.waitForDeferred(self._agent.request(request_type, url, Headers(headers),
+                                                      body_prod))
+        yield d
+        response = d.getResult()
+
+        receiver = MemoryReceiver()
+        response.deliverBody(receiver)
+
+        if request_type == 'POST' and (response.code >= 300 and response.code < 400):
+            new_location = '%s/%s' % (
+                os.path.split(url)[0],
+                response.headers.getRawHeaders('location')[0])
+            d = defer.waitForDeferred(self.get(new_location, referer))
+            yield d
+            yield d.getResult()
+            return
+        else:
+            d = defer.waitForDeferred(receiver.finished)
+            yield d
+            page = d.getResult()
+
+        yield page
+
+    def get(self, url, referer=None):
+        return self._request('GET', url, referer)
 
     def post(self, url, data, referer=None, manual_redirect=False):
         self._logger.debug('Posting to %s: %s', url, data)
-
-        headers = dict(self._POST_HEADERS)
-        if referer:
-            headers['Referer'] = referer
-
         encoded_data = urlencode(data)
-        d = getPage(
-            url,
-            method='POST',
-            headers=headers,
-            agent=self._AGENT,
-            cookies=self._cookies,
-            postdata=encoded_data,
-        followRedirect=not manual_redirect)
-
-        d.addCallback(self._on_download, RequestData(
-            urlparse(url), referer, data))
-        if manual_redirect:
-            d.addErrback(self._redirection_handler, url)
-        d.addErrback(self._on_error, url)
-        return d
-
-    def _redirection_handler(self, result, url):
-        new_location = '%s/%s' % (
-            os.path.split(url)[0],
-            result.value.location)
-        return self.get(new_location)
-
-    def _on_download(self, page, request_data):
-        if self._page_archiver:
-            reactor.callInThread(self._page_archiver.archive,
-                page, request_data.url, request_data.data, request_data.referer)
-        return page
-
-    def _on_error(self, error, url):
-        self._logger.error('Error fetching %s: %s', error, url)
-        return error
+        return self._request('POST', url, referer, encoded_data)
